34bacd58485d927fc25c033d0a34584312739c71
ghdudths1117@naver.com <ghdudths1117@naver.com>
2025-12-06 15:24:23 +0900

    1206 1523
    SearchController 수정

--- src/main/java/com/portfolio/ReadPick/controller/SearchController.java
+++ src/main/java/com/portfolio/ReadPick/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.portfolio.ReadPick.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portfolio.ReadPick.dao.SearchMapper;
+import com.portfolio.ReadPick.service.BookService;
 import com.portfolio.ReadPick.vo.BookVo;
 
 @RestController
@@ -18,16 +20,39 @@ public class SearchController {
     @Autowired
     private SearchMapper searchMapper;
 
+    @Autowired
+    private BookService bookService;
+
     @GetMapping("bookNameSearch")
     public ResponseEntity<List<BookVo>> bookNameSearch(String bookName) {
         
-        return ResponseEntity.ok(searchMapper.selectBookListByBookName(bookName));
+        List<BookVo> bookList = new ArrayList<>();
+        List<BookVo> bookIdxList = searchMapper.selectBookListByBookName(bookName);
+
+        for (BookVo book : bookIdxList) {
+            int bookIdx = book.getBookIdx();
+            String image = bookService.userGenreBookImage(bookIdx);
+            book.setBookImageName(image);
+            bookList.add(book);
+        }
+        
+        return ResponseEntity.ok(bookList);
     }
 
     @GetMapping("authorSearch")
     public ResponseEntity<List<BookVo>> authorSearch(String author) {
         
-        return ResponseEntity.ok(searchMapper.selectBookListByAuthor(author));
+        List<BookVo> bookList = new ArrayList<>();
+        List<BookVo> bookIdxList = searchMapper.selectBookListByAuthor(author);
+        
+        for (BookVo book : bookIdxList) {
+            int bookIdx = book.getBookIdx();
+            String image = bookService.userGenreBookImage(bookIdx);
+            book.setBookImageName(image);
+            bookList.add(book);
+        }
+        
+        return ResponseEntity.ok(bookList);
     }
 
     // @GetMapping("isbnSearch")
--- src/main/java/com/portfolio/ReadPick/service/BookService.java
+++ src/main/java/com/portfolio/ReadPick/service/BookService.java
@@ -1,11 +1,7 @@
 package com.portfolio.ReadPick.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.portfolio.ReadPick.dao.BookImageMapper;
 import com.portfolio.ReadPick.vo.BookImageVo;
